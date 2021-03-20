75a34a17c19fe50a33257f99816295b09c02f394
alahirajeffrey <alahirajeffrey@gmail.com>
2021-03-20 13:04:21 +0100

    added logo to script

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import datetime as dt
 from PIL import Image
@@ -7,20 +6,26 @@ from PIL import Image
 #expand page to full width
 st.set_page_config(layout="wide")
 #....................................................#
-st.title("Crypocurrency Analyzer")
-
-st.write("This web app fetches the prices of cryptocurrencies and plots them on a graph") 
 
 #....................................................#
-#Side bar 
+#load and display image
+image = Image.open("crypto_logo.jpg")
+st.image(image, width = 250)
+
+#set title 
+st.title("Crypocurrency Analyzer")
+st.write("This web app fetches the prices of cryptocurrencies and plots them on a graph") 
+#....................................................#
+
+#....................................................#
+#init sidebar 
 side = st.sidebar
 
-side.header("Input Options")
-
+#add sidebar options
+side.header("Input Details")
 crypto_ticker = side.text_input("Input cryptocurrency ticker")
 currency_ticker = side.text_input("Input currency ticker")
 year = side.text_input("Input year")
 month = side.text_input("Input month")
 day = side.text_input("Input day")
-
 #....................................................#
