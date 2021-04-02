49d23da4993512fd72a9830d60753957543bb057
alahirajeffrey <alahirajeffrey@gmail.com>
2021-04-02 14:01:20 +0100

    displaying data

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import pandas_datareader as pdr
 import streamlit as st
 import datetime as dt
@@ -15,7 +14,7 @@ image = Image.open("crypto_logo.jpg")
 st.image(image, width = 250)
 
 #set title 
-st.title("Crypocurrency Analyzer")
+st.title("Crypocurrency Trend Analyzer")
 st.write("This web app fetches the prices of cryptocurrencies and plots them on a graph") 
 #....................................................#
 
@@ -33,14 +32,19 @@ end_date = side.date_input("Select date to end analysis")
 #function to get data
 def fetch_data(ticker, currency, start_date, end_date):
     """function to fetch cryptocurrency data"""
-    scraped_data = pdr.DataReaser(f"{ticker}-{currency}","yahoo", start_date,end_date)
+    scraped_data = pdr.DataReader(f"{ticker}-{currency}","yahoo", start_date,end_date)
     return scraped_data
 
 if side.button("fetch data"):
-    data = fetch_data(crypto_ticker, currency_ticker, start_date, end_date)
+    scraped_data = fetch_data(crypto_ticker, currency_ticker, start_date, end_date)
     st.success("Data successfully scraped")
-#....................................................#
 
-#....................................................#
-#display data
+    #display needed data in dataframe
+    data = scraped_data[['High','Low','Open','Close']]
+    st.dataframe(data = data)
+
+    #display data using graph
+    st.line_chart(data, width=50, height=500)
+
+
 
